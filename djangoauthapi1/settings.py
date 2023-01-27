9d03ea656c03a6b5551050be73a6ed9e4b1642de
hamzarafiq02 <hamzarafiq9876@gmail.com>
2023-01-28 01:38:01 +0500

    remove credentials

--- djangoauthapi1/settings.py
+++ djangoauthapi1/settings.py
@@ -144,7 +144,7 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hamza99tech@gmail.com'
-EMAIL_HOST_PASSWORD = 'erzphgqzpjatbpms'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
